1d864e1499cccc71ab32e35fbce86df9664e5658
Raaa010304 <129659838+Raaa010304@users.noreply.github.com>
2023-07-20 04:53:31 +0700

    Add files via upload

---
+++ api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"eatdah/controllers/menucontroller"
+	"eatdah/controllers/menufavoritecontroller"
+	"eatdah/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	models.ConnectDatabase()
+
+	server.GET("/api/menus", menucontroller.Index)
+	server.GET("/api/menu/:id", menucontroller.Show)
+	server.POST("/api/menu", menucontroller.Create)
+	server.PUT("/api/menu/:id", menucontroller.Update)
+	server.DELETE("/api/menu", menucontroller.Delete)
+
+	server.GET("/api/menusfavorite", menufavoritecontroller.Index)
+	server.GET("/api/menufavorite/:id", menufavoritecontroller.Show)
+	server.POST("/api/menufavorite", menufavoritecontroller.Create)
+	server.PUT("/api/menufavorite/:id", menufavoritecontroller.Update)
+	server.DELETE("/api/menufavorite", menufavoritecontroller.Delete)
+
+	server.Run()
+}
